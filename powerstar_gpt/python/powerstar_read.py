282d27bf24618d5780639f5fc7edcca942256310
Thomas <tomgwasira@gmail.com>
2021-01-14 14:58:38 +0200

    Data can now be measured from DSP using .ipynb file.

--- powerstar_gpt/python/powerstar_read.py
+++ powerstar_gpt/python/powerstar_read.py
@@ -7,5 +7,19 @@
 # Version: 1.0
 
 import matplotlib.pyplot as plt
-import numpy as
+import numpy as np
 import serial
+
+#---------------------------------------------------------------
+# Reading data from Powerstar 10 card
+#---------------------------------------------------------------
+
+# Setup primary UART (miniUART) and read from Powerstar card
+with serial.Serial("/dev/serial0", 115200, timeout=1) as ser:
+    ser.reset_input_buffer() # clear input serial buffer
+    ser.reset_output_buffer() # clear output serial buffer
+    ser.write(b"-twh\r") # request headings for signals from Powerstar 10 card
+    headings = str(ser.read(1024)) # read serial data in response to headings request. strip() necessary for any leading and trailing spaces in data
+    headings = headings.split('r')
+    print(headings)
+    ser.close()
